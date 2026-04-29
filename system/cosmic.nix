f74884ecb99701dd7500e727ae70eeccb28dde7c
Fia <38019116+ChaoticNeutralGnome@users.noreply.github.com>
2026-04-29 12:06:46 -0400

    removed unused flag

--- system/cosmic.nix
+++ system/cosmic.nix
@@ -5,11 +5,6 @@
     services.desktopManager.cosmic.enable = true;
     services.system76-scheduler.enable = true;
 
-    # Disable Variable Refresh Rate
-    environment.sessionVariables = {
-        COSMIC_DISABLE_VRR = "1";
-    };
-
     # Use Wayland for Chromium/Electron Applications
     environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
