d0dd43f1f10cf72b11af60843f227ccb9306e4a8
Fia <38019116+ChaoticNeutralGnome@users.noreply.github.com>
2026-07-20 20:01:21 -0400

    firefox settings tweaks

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -9,15 +9,20 @@
 
             settings = {
                 "browser.download.panel.shown" = true;
+
                 "browser.newtabpage.activity-stream.enabled" = true;
                 "browser.newtabpage.activity-stream.default.sites" = "";
                 "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
-                "browser.newtabpage.activity-stream.feeds.topsites" = false;
+                "browser.newtabpage.activity-stream.feeds.topsites" = true;
+                "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
                 "browser.newtabpage.activity-stream.discoverystream.sendToPocket.enabled" = false;
-                "extensions.autoDisableScopes" = 0;
                 "browser.newtabpage.activity-stream.feeds.wallpaperfeed" = true;
                 "browser.newtabpage.activity-stream.newtabWallpapers.customWallpaper.enabled" = true;
                 "browser.newtabpage.activity-stream.newtabWallpapers.wallpaper" = "abstract-blue-purple";
+
+                "browser.compactmode.show" = true;
+
+                "extensions.autoDisableScopes" = 0;
             };
 
             extensions.packages = with pkgs.firefox-addons; [
@@ -31,6 +36,21 @@
                 betterttv
                 beyond-20
             ];
+
+            userContent = ''
+                @-moz-document url("about:newtab"), url("about:home") {
+                .top-site-outer .tile .icon-wrapper,
+                .top-site-outer .default-icon {
+                    width: 80px !important;
+                    height: 80px !important;
+                    background-size: cover !important;
+                }
+
+                .top-site-outer {
+                    width: 90px !important;
+                }
+                }
+            '';
         };
     };
 }
